63684a19b9b80b6d2d22b4517d6225c214d28224
ColNight8107 <a9609106@gmail.com>
2017-04-23 00:20:16 +0800

    推送plugins＆新增Youtube通知功能

---
+++ youtube/webview.js
@@ -0,0 +1,27 @@
+module.exports = (Franz, options) => {
+  const getValueById = (id) => {
+    return document.getElementById(id).innerText;
+  }
+  const getMessages = () => {
+    const notifications = getValueById('yt-masthead-notifications');
+    Franz.setBadge(notifications);
+  }
+
+  Franz.loop(getMessages);
+
+  // const getValueById = (id) => 
+  // {
+  // 	return document.getElementById(id).innerText;
+  // };
+  // const getMessages = () => 
+  // {
+  // 	const unreadNotices = getValueById('notificationsCountValue');
+  // 	const unreadMessage = getValueById('mercurymessagesCountValue');
+  // 	Franz.setBadge(unreadMessage, unreadNotices);
+  // }
+  // Franz.loop(getMessages);
+  // // Franz.injectCSS(path.join(__dirname, "style.css"));
+  // document.getElementById('rightCol').remove();
+  // document.getElementById('stream_pagelet').style.width = '150%';
+  // document.getElementById('mainContainer').style.paddingLeft = '100px';
+}
